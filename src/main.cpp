193a942db3fb8c10fdb04ce64975618f41658abf
Jeramy Harrison <dbralir@gmail.com>
2015-12-13 07:43:59 -0430

    Added treasure and branching

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
 #include <chrono>
 #include <random>
 #include <array>
+#include <thread>
 
 using PRNG = std::mt19937_64;
 static auto rng = []{
@@ -93,9 +94,10 @@ Overloaded<R,Ts...> overload(Ts&&... ts) {
 }
 
 struct Game {
-    static constexpr auto player_speed = 3.f;
+    static constexpr auto player_speed = 2.f;
 
-    void (Game::*cur_state)(double);
+    using State = void(Game::*)(double);
+    State cur_state;
 
     glm::vec3 player_pos = {0.f, 0.f, 0.f};
     glm::quat player_rot = glm::quat();
@@ -108,7 +110,17 @@ struct Game {
         sushi::compile_shader_file(sushi::shader_type::FRAGMENT, "assets/shaders/fragment.glsl")
     });
 
-    std::shared_ptr<Hallway> cur_hall = std::make_shared<Hallway>(default_hall);
+    sushi::static_mesh spriteobj = sushi::load_static_mesh_data(
+        {{-1,1,0},{1,1,0},{-1,-1,0},{1,-1,0}},
+        {{0,0,1}},
+        {{0,1},{1,1},{0,0},{1,0}},
+        {{{{0,0,0},{1,0,1},{2,0,2}}},{{{2,0,2},{1,0,1},{3,0,3}}}}
+    );
+
+    sushi::static_mesh treasureobj = sushi::load_static_mesh_file("assets/models/treasure.obj");
+    sushi::texture_2d treasuretex = sushi::load_texture_2d("assets/textures/treasure.png", false, false);
+
+    std::shared_ptr<Hallway> cur_hall = std::make_shared<Hallway>(Hallway{5,Treasure{},{},{}});
 
     glm::mat4 proj_mat = glm::perspectiveFov(glm::radians(120.f), 4.f, 3.f, 0.01f, 50.f);
     glm::mat4 view_mat;
@@ -117,9 +129,13 @@ struct Game {
 
     LightSource lamp = LightSource(2.5, 3);
 
-    Game(sushi::window* window) : cur_state(state_moving), window(window) {}
+    Game(sushi::window* window) : cur_state(state_moving), window(window) {
+        cur_hall->left = std::make_shared<Hallway>(Hallway{5,{},{},{}});
+        cur_hall->right = std::make_shared<Hallway>(Hallway{5,Treasure{},{},{}});
+    }
 
     glm::mat4 draw_hallway(const Hallway& hall, glm::mat4 model_mat) {
+        sushi::set_texture(0, halltex);
         for (int i=0; i<hall.len; ++i) {
             auto mvp = proj_mat * view_mat * model_mat;
             sushi::set_uniform(shader, "MVP", mvp);
@@ -154,7 +170,15 @@ struct Game {
         }
         boost::apply_visitor(overload<void>(
             [&](const Nothing&){},
-            [&](const Treasure&){}
+            [&](const Treasure&){
+                auto treasure_mat = glm::translate(model_mat, {0.f, 0.f, 0.42265});
+                auto mvp = proj_mat * view_mat * treasure_mat;
+                sushi::set_uniform(shader, "MVP", mvp);
+                sushi::set_uniform(shader, "ModelMat", treasure_mat);
+                sushi::set_uniform(shader, "ViewMat", view_mat);
+                sushi::set_texture(0, treasuretex);
+                sushi::draw_mesh(treasureobj);
+            }
         ), hall.inhabitant);
     }
 
@@ -162,7 +186,6 @@ struct Game {
         sushi::set_program(shader);
 
         sushi::set_uniform(shader, "Texture", 0);
-        sushi::set_texture(0, halltex);
 
         if (window->is_down(sushi::input_button{sushi::input_type::KEYBOARD, GLFW_KEY_F1})) {
             sushi::set_uniform(shader, "FullBright", 1);
@@ -183,7 +206,10 @@ struct Game {
 
         if (until_stop < step_size) {
             player_pos.z += until_stop;
-            cur_state = state_event;
+            cur_state = boost::apply_visitor(overload<State>(
+                [&](const Nothing&){ return &state_tojunc; },
+                [&](const Treasure&){ return &state_treasure; }
+            ), cur_hall->inhabitant);
         } else {
             player_pos.z += step_size;
         }
@@ -195,9 +221,88 @@ struct Game {
         draw_hallway_full(*cur_hall, model_mat);
     }
 
-    void state_event(double delta) {
+    void state_tojunc(double delta) {
+        auto until_stop = cur_hall->len * 2.f - 0.4226497f - player_pos.z;
+        auto step_size = delta * player_speed;
+
+        if (until_stop < step_size) {
+            player_pos.z += until_stop;
+            cur_state = state_whichway;
+        } else {
+            player_pos.z += step_size;
+        }
+
         view_mat = mat4_cast(player_rot) * glm::mat4(1.f);
         view_mat = glm::translate(view_mat, player_pos);
+
+        auto model_mat = glm::mat4(1.f);
+        draw_hallway_full(*cur_hall, model_mat);
+    }
+
+    void state_turnleft(double delta) {
+        auto until_stop = glm::radians(-60.f) - glm::yaw(player_rot);
+        auto step_size = -float(delta * player_speed);
+
+        if (until_stop > step_size) {
+            player_rot *= glm::angleAxis(until_stop, glm::vec3{0.f, 1.f, 0.f});
+            cur_hall = cur_hall->left;
+            cur_hall->left = std::make_shared<Hallway>(Hallway{5, {}, {}, {}});
+            cur_hall->right = std::make_shared<Hallway>(Hallway{5, Treasure{}, {}, {}});
+            player_pos.z = -1.57735f;
+            player_rot = glm::quat();
+            cur_state = state_moving;
+        } else {
+            player_rot *= glm::angleAxis(step_size, glm::vec3{0.f, 1.f, 0.f});
+        }
+
+        view_mat = mat4_cast(player_rot) * glm::mat4(1.f);
+        view_mat = glm::translate(view_mat, player_pos);
+        auto model_mat = glm::mat4(1.f);
+        draw_hallway_full(*cur_hall, model_mat);
+    }
+
+    void state_turnright(double delta) {
+        auto until_stop = glm::radians(60.f) - glm::yaw(player_rot);
+        auto step_size = float(delta * player_speed);
+
+        if (until_stop < step_size) {
+            player_rot *= glm::angleAxis(until_stop, glm::vec3{0.f, 1.f, 0.f});
+            cur_hall = cur_hall->right;
+            cur_hall->left = std::make_shared<Hallway>(Hallway{5, Treasure{}, {}, {}});
+            cur_hall->right = std::make_shared<Hallway>(Hallway{5, {}, {}, {}});
+            player_pos.z = -1.57735f;
+            player_rot = glm::quat();
+            cur_state = state_moving;
+        } else {
+            player_rot *= glm::angleAxis(step_size, glm::vec3{0.f, 1.f, 0.f});
+        }
+        view_mat = mat4_cast(player_rot) * glm::mat4(1.f);
+        view_mat = glm::translate(view_mat, player_pos);
+        auto model_mat = glm::mat4(1.f);
+        draw_hallway_full(*cur_hall, model_mat);
+    }
+
+    void state_whichway(double delta) {
+        view_mat = mat4_cast(player_rot) * glm::mat4(1.f);
+        view_mat = glm::translate(view_mat, player_pos);
+
+        if (window->was_pressed(sushi::input_button{sushi::input_type::KEYBOARD, GLFW_KEY_LEFT})) {
+            cur_state = state_turnleft;
+        }
+        if (window->was_pressed(sushi::input_button{sushi::input_type::KEYBOARD, GLFW_KEY_RIGHT})) {
+            cur_state = state_turnright;
+        }
+
+        auto model_mat = glm::mat4(1.f);
+        draw_hallway_full(*cur_hall, model_mat);
+    }
+
+    void state_treasure(double delta) {
+        view_mat = mat4_cast(player_rot) * glm::mat4(1.f);
+        view_mat = glm::translate(view_mat, player_pos);
+
+        cur_hall->inhabitant = Nothing{};
+        cur_state = state_tojunc;
 
         auto model_mat = glm::mat4(1.f);
         draw_hallway_full(*cur_hall, model_mat);
